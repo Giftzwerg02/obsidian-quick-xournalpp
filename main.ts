c41b0db5d0378bcfc11160e8d18ac1dc2b6c0baf
Benjamin <benjamin.komar@kabsi.at>
2023-02-06 18:06:01 +0100

    basic poc for this plugin

--- main.ts
+++ main.ts
@@ -1,137 +1,78 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, FileSystemAdapter, MarkdownView, Modal, Plugin, Plugin_2, Setting } from "obsidian";
+import { exec } from "child_process";
 
-// Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+class XournalPPCreateModal extends Modal {
+	result: string;
+	onSubmit: (result: string) => void;
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
+	constructor(app: App, onSubmit: (result: string) => void) {
 		super(app);
+		this.onSubmit = onSubmit;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		let { contentEl } = this;
+		contentEl.createEl("h1", { text: "File name" });
+		new Setting(contentEl)
+			.setName("filename")
+			.addText((text) => {
+				text.onChange((value) => {
+					this.result = value
+				})
+			})
+
+		new Setting(contentEl)
+			.addButton((btn) => {
+				btn
+					.setButtonText("Submit")
+					.setCta()
+					.onClick(() => {
+						this.close();
+						if (!this.result.endsWith(".xopp")) {
+							this.result += ".xopp";
+						}
+						this.onSubmit(this.result);
+					})
+			})
 	}
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+	onClose(): void {
+		let { contentEl } = this;
+		contentEl.empty(); 
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export default class ExamplePlugin extends Plugin {
+  async onload() {
+    this.addCommand({
+      id: "create-and-link-xournalpp-file",
+      name: "Create and Link Xournal++ file",
+      editorCallback: (editor: Editor, view: MarkdownView) => {
+		new XournalPPCreateModal(this.app, async (result) => {
+			const fs = this.app.vault.adapter as FileSystemAdapter;
+			try {
+				const template_file = getPluginDir(this, "empty.xopp"); 
+				const new_file = `resources/${result}`; 
+				await fs.copy(
+					template_file,
+					new_file	
+				)
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+				const new_file_fullpath = fs.getFullPath(new_file);
+				const cmd = `xournalpp -p "${new_file_fullpath}.pdf" "${new_file_fullpath}"`
+				exec(cmd, (err, stdout, stderr) => {					
+					editor.replaceSelection(`![[${new_file}]]\n![[${new_file}.pdf]]`)
+				});
+			} catch (e) {
+				console.log(e);
+			}	
+		}).open();
+      },
+    });
+  }
+}
 
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+function getPluginDir(plug: Plugin_2, asset: string) {
+	return `${plug.app.vault.configDir}/plugins/${plug.manifest.id}/${asset}`;
 }
