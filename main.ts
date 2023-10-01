c47e9421c246119095cf144ba9d48d33bbf35de8
Giftzwerg02 <benjamin.komar@kabsi.at>
2023-10-01 19:47:42 +0200

    Made resource folder configurable and added simple error reporting to the plugin modal.

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
-import { App, Editor, FileSystemAdapter, MarkdownView, Modal, Plugin, Plugin_2, Setting } from "obsidian";
+import { App, Editor, FileSystemAdapter, MarkdownView, Modal, Plugin, PluginSettingTab, Plugin_2, Setting } from "obsidian";
 import { exec } from "child_process";
 
 
 class XournalPPCreateModal extends Modal {
 	result: string;
-	onSubmit: (result: string) => void;
+	plugin: ExamplePlugin;
+	onSubmit: (result: string) => Promise<void>;
 
-	constructor(app: App, onSubmit: (result: string) => void) {
+	constructor(app: App, plugin: ExamplePlugin, onSubmit: (result: string) => Promise<void>) {
 		super(app);
+		this.plugin = plugin;
 		this.onSubmit = onSubmit;
 	}
 
@@ -20,57 +22,121 @@ class XournalPPCreateModal extends Modal {
 				text.onChange((value) => {
 					this.result = value
 				})
-			})
+			});
+
+		let errorDiv: HTMLDivElement | null = null;
 
 		new Setting(contentEl)
 			.addButton((btn) => {
 				btn
 					.setButtonText("Submit")
 					.setCta()
-					.onClick(() => {
-						this.close();
+					.onClick(async () => {
 						if (!this.result.endsWith(".xopp")) {
 							this.result += ".xopp";
 						}
-						this.onSubmit(this.result);
+						try {
+							await this.onSubmit(this.result);
+							this.close();
+						}
+						catch (e) {
+							if(errorDiv === null) {
+								errorDiv = contentEl.createDiv();
+							}
+							errorDiv.innerHTML = "";	
+							errorDiv.createEl("h2", { text: "Error!" });
+
+							if(e.code === "ENOENT" && e.syscall === "copyfile") {
+								errorDiv.createEl("strong", { text: "Your resource-location probably does not exist, please set its value to a folder in your vault." })
+								let p = errorDiv.createEl("p");
+								p.innerHTML = `Currently set location: <strong>${this.plugin.settings.resourceLocation}</strong>`
+							}
+
+							errorDiv.createEl("pre", { attr: { style: "white-space: pre-wrap;" } }).createEl("p", { text: e });
+						}
 					})
 			})
 	}
 
 	onClose(): void {
 		let { contentEl } = this;
-		contentEl.empty(); 
+		contentEl.empty();
 	}
 }
 
-export default class ExamplePlugin extends Plugin {
-  async onload() {
-    this.addCommand({
-      id: "create-and-link-xournalpp-file",
-      name: "Create and Link Xournal++ file",
-      editorCallback: (editor: Editor, view: MarkdownView) => {
-		new XournalPPCreateModal(this.app, async (result) => {
-			const fs = this.app.vault.adapter as FileSystemAdapter;
-			try {
-				const template_file = getPluginDir(this, "empty.xopp"); 
-				const new_file = `resources/${result}`; 
-				await fs.copy(
-					template_file,
-					new_file	
-				)
+export class ExampleSettingsTab extends PluginSettingTab {
+	plugin: ExamplePlugin;
 
-				const new_file_fullpath = fs.getFullPath(new_file);
-				const cmd = `xournalpp -p "${new_file_fullpath}.pdf" "${new_file_fullpath}"`
-				exec(cmd, (err, stdout, stderr) => {					
-					editor.replaceSelection(`![[${new_file}]]\n![[${new_file}.pdf]]`)
-				});
-			} catch (e) {
-				console.log(e);
-			}	
-		}).open();
-      },
-    });
-  }
+	constructor(app: App, plugin: ExamplePlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display() {
+		let { containerEl } = this;
+
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName("Resource Location")
+			.setDesc("The location where your generated xournal++ and pdf-files are saved to.")
+			.addText((text) =>
+				text
+					.setPlaceholder("e.g. 'resources'")
+					.setValue(this.plugin.settings.resourceLocation)
+					.onChange(async (value) => {
+						this.plugin.settings.resourceLocation = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+}
+
+interface ExamplePluginSettings {
+	resourceLocation: string;
+}
+
+const DEFAULT_SETTINGS: ExamplePluginSettings = {
+	resourceLocation: "resources"
+}
+
+export default class ExamplePlugin extends Plugin {
+	settings: ExamplePluginSettings;
+
+	async loadSettings() {
+		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
+	async onload() {
+		await this.loadSettings();
+		this.addSettingTab(new ExampleSettingsTab(this.app, this));
+
+		this.addCommand({
+			id: "create-and-link-xournalpp-file",
+			name: "Create and Link Xournal++ file",
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				new XournalPPCreateModal(this.app, this, async (result) => {
+					const fs = this.app.vault.adapter as FileSystemAdapter;
+					const template_file = getPluginDir(this, "empty.xopp");
+					const new_file = `${this.settings.resourceLocation}/${result}`;
+					await fs.copy(
+						template_file,
+						new_file
+					)
+
+					const new_file_fullpath = fs.getFullPath(new_file);
+					const cmd = `xournalpp -p "${new_file_fullpath}.pdf" "${new_file_fullpath}"`
+					exec(cmd, (err, stdout, stderr) => {
+						editor.replaceSelection(`![[${new_file}]]\n![[${new_file}.pdf]]`)
+					});
+				}).open();
+			},
+		});
+	}
 }
 
 function getPluginDir(plug: Plugin_2, asset: string) {
